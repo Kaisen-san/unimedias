73eaacdb9b968d3c306d727cff932804060aa177
Felipe Andrade <fasa.work@gmail.com>
2019-02-11 00:00:27 -0200

    Add first routes

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const childProcess = require('child_process');
 const phantomjs = require('phantomjs-prebuilt');
+const port = process.env.port || 4000;
+const app = express();
 
-let childArgs = [
-	path.join(__dirname, 'robot.js'),
-	'<USER_EMAIL>',
-	'<USER_PASSWORD>'
-]
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-childProcess.execFile(
-	phantomjs.path,
-	childArgs,
-	(err, stdout, stderr) => {
-		if (err) {
-			console.log(stderr);
-			return;
-		}
+let subjects = [];
 
-		let output = JSON.parse(stdout);
+app.get('/', (req, res) => res.render('pages/index'));
 
-		if (output.error) {
-			console.log(output.error);
-			return;
-		}
+app.get('/login', (req,res) => res.render('pages/main', { 'subjects': subjects }));
 
-		console.log(output.data);
-	});
+app.post('/login', (req, res) => {
+	let childArgs = [
+		path.join(__dirname, 'robot.js'),
+		req.body.email,
+		req.body.pass
+	]
+	
+	childProcess.execFile(
+		phantomjs.path,
+		childArgs,
+		(err, stdout, stderr) => {
+			if (err) {
+				console.log(stderr);
+				return;
+			}
+			
+			let output = JSON.parse(stdout);
+			
+			if (output.error) {
+				console.log(output.error);
+				return;
+			}
+			
+			subjects = output.data;
+			res.redirect('/login');
+		});
+});
+
+const server = app.listen(port);
+console.log(`Express server listening at port ${server.address().port}`);
